62073cae0ed10949840251ee6e37fdc6c4fc01c6
Deepak Ravi Mishra <deepak.10733160@ltimindtree.com>
2024-10-24 19:13:12 +0530

    added counter store

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CounterComponent } from './component/counter/counter.component';
+import { Observable } from 'rxjs';
+import { AppState } from './state/app.state';
+import { Store } from '@ngrx/store';
+import { selectCount } from './state/counter/counter.selector';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AsyncPipe, CounterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'angular-version-18';
+  title = 'angular-state-ngrx';
+
+  count: Observable<number>;
+  constructor(private counterStore: Store<AppState>) {
+    this.count = counterStore.select(selectCount);
+  }
 }
